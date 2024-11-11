bc19b1f335255488ae7b714e0935dfd4fd603787
ZongqiLiuphd <862426223@qq.com>
2024-11-11 21:19:31 +0800

    first commit

--- GAN-MMC.py
+++ GAN-MMC.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import torchvision.utils
 import numpy as np
 import random
+import time
 from PIL import Image
 import torch
 from torch.autograd import Variable
@@ -17,11 +18,10 @@ import PIL.ImageOps
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
-from audtorch.metrics.functional import pearsonr
 
 class MFS_Missing_Dataset(Dataset):
 
-    def __init__(self, dataset, num, p_miss, M):
+    def __init__(self, dataset, num, M):
         M_H = np.loadtxt(str(dataset) + '_H_200.csv', delimiter=",", skiprows=0)
         M_L = np.loadtxt(str(dataset) + '_L_200.csv', delimiter=",", skiprows=0)
         m = len(M_H)
@@ -75,7 +75,6 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
 
-        ## 模型中间块儿
         def block(in_feat, out_feat, normalize=False):
             layers = [nn.Linear(in_feat, out_feat)]
             if normalize:
@@ -83,19 +82,30 @@ class Discriminator(nn.Module):
             layers.append(nn.LeakyReLU())
             return layers
 
-        ## prod():返回给定轴上的数组元素的乘积:1*28*28=784
         self.model = nn.Sequential(
-            *block(Config.Data_area, 128, normalize=False),
-            *block(128, 256),
-            *block(256, 128),
-            nn.Linear(128, 1),
+            *block(8*Config.Data_area, 256, normalize=False),
+            nn.Linear(256, 1),
             nn.Sigmoid()
         )
+        self.cnn1 = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(1, 4, kernel_size=3),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(4),
 
-    def forward(self, x_noise):
-        x_noise = x_noise.view(x_noise.size()[0], Config.Data_area)
-        x = self.model(x_noise)
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(4, 8, kernel_size=3),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(8),
+        )
+
+    def forward(self, x):
+        x = x.view(x.size()[0], *Config.Data_size)
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)
+        x = self.model(output)
         return x
+
 
 class Generator(nn.Module):
     def __init__(self):
@@ -106,19 +116,36 @@ class Generator(nn.Module):
                 layers.append(nn.BatchNorm1d(out_feat, 0.8))
             layers.append(nn.LeakyReLU())
             return layers
+
         self.model = nn.Sequential(
             *block(Config.Data_area, 128, normalize=False),
             *block(128, 256),
-            #*block(256, 256),
             *block(256, 128),
             nn.Linear(128, Config.Data_area),
             nn.Sigmoid()
         )
-    def forward(self, x_noise):
-        x_noise = x_noise.view(x_noise.size()[0],Config.Data_area)
-        x = self.model(x_noise)
-        x = x.view(x.size(0), *Config.Data_size)
-        return x
+
+        self.cnn1 = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(1, 4, kernel_size=3),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(4),
+
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(4, 4, kernel_size=3),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(4),
+
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(4, 1, kernel_size=3),
+            nn.LeakyReLU(inplace=True),
+            nn.BatchNorm2d(1),
+        )
+
+    def forward(self, x):
+        x = x.view(x.size()[0], *Config.Data_size)
+        output = self.cnn1(x)
+        return output
 
 class Gan():
     def __init__(self, dataset, train_batch_size, train_number_epochs, Data_size, alpha, beta, hnum):
@@ -135,8 +162,8 @@ class Gan():
         else:
             self.generator = Generator()
             self.discriminator = Discriminator()
-        self.optimizer_G = torch.optim.Adam(self.generator.parameters(), lr=0.001)
-        self.optimizer_D = torch.optim.Adam(self.discriminator.parameters(), lr=0.0001)
+        self.optimizer_G = torch.optim.Adam(self.generator.parameters(), lr=Config.lr_G)
+        self.optimizer_D = torch.optim.Adam(self.discriminator.parameters(), lr=Config.lr_D)
 
     def discriminator_loss(self,LF_X, real_X, M):
         # Generator
@@ -156,6 +183,7 @@ class Gan():
         return D_loss
 
     def generator_loss(self, LF_X, real_X, M):
+        # %% Structure
         # Generator
         G_sample = self.generator(LF_X)
 
@@ -168,10 +196,10 @@ class Gan():
         real_label = Variable(torch.ones(LF_X.size(0), 1)).cuda()
         criterion = torch.nn.BCELoss()
         G_loss1 = criterion(D_prob, real_label)
-
         similarity1 = pearsonr(Hat_New_X.view(LF_X.size()[0], -1), LF_X.view(LF_X.size()[0], -1))[:, 0]
         similarity2 = pearsonr((Hat_New_X * M).view(LF_X.size()[0], -1), (LF_X * M).view(LF_X.size()[0], -1))[:, 0]
         G_loss2 = torch.abs(torch.mean(similarity1 - similarity2))
+
         MSE_train_loss = torch.mean((M * real_X - M * G_sample) ** 2) / torch.mean(M)
 
         G_loss = G_loss1 + self.alpha * MSE_train_loss + self.beta * G_loss2
@@ -185,7 +213,6 @@ class Gan():
         for epoch in range(0, self.train_number_epochs):
             for i, data in enumerate(train_dataloader, 0):
                 X_H, X_L, M = data
-
                 if Config.use_gpu is True:
                     X_H = Variable(X_H.view(X_H.size()[0], *self.Data_size)).cuda().to(torch.float32)
                     X_L = Variable(X_L.view(X_H.size()[0], *self.Data_size)).cuda().to(torch.float32)
@@ -222,34 +249,39 @@ class Gan():
 
 
 class Config():
-    #name = 'shubert'
     num = 10000
-    p_miss = 0.975
     alpha = 200
     train_batch_size = 256
     train_number_epochs = 100
     Data_size = (1, 20, 10)
     Data_area = 200
     beta = 100
+    lr_G = 0.0001
+    lr_D = 0.005
     use_gpu = True  # set it to True to use GPU and False to use CPU
 
 if __name__ == '__main__':
     if Config.use_gpu:
         torch.cuda.set_device(0)
 
-    name = 'beale'
-    for hnum in range(3, 31):
-        M_all = np.loadtxt('.\M\hnum='+ str(round(hnum,3)) + '_H_200.csv', delimiter=",", skiprows=0)
-        Y = np.zeros((Config.Data_area, 10))
-        for l in range(10):
-            M = M_all[:,l].reshape(20,10)
+    for name in ['beale']:
+        for hnum in range(3,31):
+            M_all = np.loadtxt('.\M\hnum='+ str(round(hnum,3)) + '_H_200.csv', delimiter=",", skiprows=0)
+            Y = np.zeros((Config.Data_area, 10))
+            for l in range(10):
+                M = M_all[:,l].reshape(20,10)
 
-            dataset = MFS_Missing_Dataset(name, Config.num, Config.p_miss, M)
-            GAN = Gan(dataset, Config.train_batch_size, Config.train_number_epochs, Config.Data_size, Config.alpha, Config.beta, hnum)
-            GAN.train()
-            y = GAN.test()
-            Y[:, l] = y.detach().numpy().reshape(-1,)
-        np.savetxt('./GAN-MMC/'+name+'hnum='+str(round(hnum,3))+'_20-10.csv', Y, delimiter=',')
+                dataset = MFS_Missing_Dataset(name, Config.num, M)
+                GAN = Gan(dataset, Config.train_batch_size, Config.train_number_epochs, Config.Data_size, Config.alpha, Config.beta, hnum)
+                t1=time.time()
+                GAN.train()
+                t2=time.time()
+                y = GAN.test()
+                t3=time.time()
+                # print('训练时间',t2-t1)
+                # print('测试时间', t3 - t2)
+                Y[:, l] = y.detach().numpy().reshape(-1,)
+            np.savetxt('./GAN-MMC/'+name+'hnum='+str(round(hnum,3))+'_200.csv', Y, delimiter=',')
 
 
 
